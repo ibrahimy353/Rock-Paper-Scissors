e3b0c443994502df50ab3d088b80006d57a44553
ibrahimy353 <ihusssein@gmail.com>
2022-01-24 05:02:35 +0300

    i am commiting multiple files

--- main.js
+++ main.js
@@ -74,7 +74,6 @@ function game() {
   let computerWins = 0;
   let playRoundReturn = 0;
 
-  while ( round <= 3 ) {
 
     playRoundReturn = playRound(playerSelection(), computerSelection());
     if ( playRoundReturn === true ) {
